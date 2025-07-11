99739d1428aba7d49e60365e034bf2ebd1a2c770
mar0303 <scherfmarilis@gmail.com>
2025-07-11 14:22:35 +0200

    remove unused imports

--- app/src/main/java/com/example/happyplaces2/MeineMap.kt
+++ app/src/main/java/com/example/happyplaces2/MeineMap.kt
@@ -5,10 +5,7 @@ import org.osmdroid.config.Configuration
 import android.os.Bundle
 import androidx.activity.compose.setContent
 import androidx.appcompat.app.AppCompatActivity
-import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Scaffold
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalContext
